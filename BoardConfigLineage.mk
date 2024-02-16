906d611fa4dc319276fd2726a95760d57198b32a
Oscar <osm1019@gmail.com>
2024-02-15 18:25:49 -0800

    fix up! derp => lineage

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -11,7 +11,7 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/derp/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     device/google/zuma/manifest_radio_ds.xml
 
--- device.mk
+++ device.mk
@@ -898,7 +898,7 @@ endif
 
 PRODUCT_PACKAGES += ShannonRcs
 
-ifeq ($(DERP_BUILD),)
+ifeq ($(LINEAGE_BUILD),)
 ifeq (,$(filter aosp_% factory_%,$(TARGET_PRODUCT)))
 #ImsMediaAoc library
 FEATURE_TYPE := oem_audio
