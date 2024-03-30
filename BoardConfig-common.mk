9a6fc55532a4b4fccee89001749862b9b3d6bc5a
Oscar <osm1019@gmail.com>
2024-03-30 10:20:53 -0700

    zuma:  set BUILD_BROKEN_DUP_RULES := true
    Change-Id: Iaf82bcbbf210c9a49844b7e8fded2452b7c13376

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -16,6 +16,9 @@
 include build/make/target/board/BoardConfigMainlineCommon.mk
 include build/make/target/board/BoardConfigPixelCommon.mk
 
+BUILD_BROKEN_DUP_RULES := true
+
+
 # Should be uncommented after fixing vndk-sp violation is fixed.
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
